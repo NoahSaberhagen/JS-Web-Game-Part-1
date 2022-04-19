4d96112a5e0004a44e6f4e1612a8cf1e38651316
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-04-19 19:52:24 -0400

    created newImg function

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+const newImg = (name, source, left, bottom) => {
+    let name = document.createElement('img');
+    name.src = = source;
+    name.style.position = 'fixed';
+    name.style.left = left;
+    name.style.bottom = bottom;
+    document.body.append(name);
+};
+
+newImg(greenCharacter, 'assets/green-character.gif', '100px', '100px');
+newImg(treeSprite, 'assets/pine-tree.png', '450px', '200px');
