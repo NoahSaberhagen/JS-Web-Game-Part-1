f5b6a95d517df3e952395b2e1a049b8b04858b67
NoahSaberhagen <noahsaberhagen@gmail.com>
2022-04-19 20:21:05 -0400

    added more images

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function newImage(y, l, b) {
 
 newImage('assets/green-character.gif', '100px', '100px');
 newImage('assets/pine-tree.png', '450px', '200px');
+newImage('assets/pillar.png', '350px', '100px');
+newImage('assets/crate.png','150px','200px');
+newImage('assets/well.png','500px','425px');
+newImage('assets/tree.png','200px','300px');
